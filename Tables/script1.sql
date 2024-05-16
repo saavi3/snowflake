02a41d0d5e79257e7d73834462c67566d7343f28
Lakshika Savithri <savithriamarasinghe@gmail.com>
2024-05-16 07:38:13 +0530

    COMMIT-2024-05-16-7:38

---
+++ Tables/script1.sql
@@ -0,0 +1,9 @@
+/* Restores the most recent version of a dropped table. 
+
+TRY THIS : Specifies the identifier for the table to restore.
+ If the identifier contains spaces or special characters, the entire string must be enclosed in double quotes. 
+Identifiers enclosed in double quotes are also case-sensitive.
+
+https://docs.snowflake.com/en/sql-reference/sql/undrop-table
+*/
+UNDROP TABLE <name>
